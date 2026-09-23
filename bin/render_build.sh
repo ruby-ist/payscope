e5c126a30b8f41919c95ac1004e7838fb6840ede
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2026-09-23 20:57:49 +0530

    Added steps in `bin/render_build.sh` to seed the prod database

--- bin/render_build.sh
+++ bin/render_build.sh
@@ -7,3 +7,7 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 mkdir -p "$HOME/.postgresql" && curl -o "$HOME/.postgresql/root.crt" "https://cockroachlabs.cloud/clusters/7ae464c2-e293-4708-957a-373ade476566/cert"
 bundle exec rake db:migrate
+
+# steps to seed the database
+script/generate_seed_data --exchange-rates 8 --employees 10100 --invalid-employees 100
+rails db:seed
